98bfe0a47a5a385aa5e510338a94fcafdc20bcc3
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-11-14 22:20:33 +0200

    Some more

--- typescript/packages/frontend-main/pages/index.tsx
+++ typescript/packages/frontend-main/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-// import { getLowo } from "@oyelowo/web-main";
+import { getLowo, TextField } from "@oyelowo/lib-core";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +13,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-
+        another name {getLowo()}
+        Home of grind!
+        <TextField label="Oyelowo" description="The sweet field" errorMessage="Bad error" />
       </main>
 
       <footer>
