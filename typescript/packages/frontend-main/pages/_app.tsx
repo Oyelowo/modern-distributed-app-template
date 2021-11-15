35917791fa0cd2c808ea3c56412fe52418c0fcd6
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-11-16 00:37:42 +0200

    Install twin macro in frontend workspace

--- typescript/packages/frontend-main/pages/_app.tsx
+++ typescript/packages/frontend-main/pages/_app.tsx
@@ -1,8 +1,12 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
+import { GlobalStyles } from "twin.macro";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <div>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </div>
+);
 
-export default MyApp;
+export default App;
