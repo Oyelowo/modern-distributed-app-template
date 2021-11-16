af94b917d4de4d599d395a403f1d56ce36b6a90f
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-11-16 06:25:21 +0200

    twin.macro + storybook

--- typescript/packages/lib-core/styles/GlobalStyles.tsx
+++ typescript/packages/lib-core/styles/GlobalStyles.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Global } from "@emotion/react";
-import tw, { theme, GlobalStyles as BaseStyles } from "twin.macro";
+import tw, { css, theme, GlobalStyles as BaseStyles } from "twin.macro";
 
-// const customStyles = css`
-//   body {
-//     -webkit-tap-highlight-color: ${theme`colors.purple.500`};
-//     ${tw`antialiased`}
-//   }
-// `;
+const customStyles = css`
+  body {
+    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
+    ${tw`antialiased`}
+  }
+`;
 
 const GlobalStyles = () => (
   <>
     <BaseStyles />
-    <Global styles={{}} />
+    <Global styles={customStyles} />
   </>
 );
 
