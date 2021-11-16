2a0ace60c7f0708bc9cb03183c54e4fd711e7ffa
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-11-16 06:22:08 +0200

    Install twin.macro for storybook in lib-core

--- typescript/packages/lib-core/pages/_app.tsx
+++ typescript/packages/lib-core/pages/_app.tsx
@@ -1,8 +1,12 @@
-import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import { GlobalStyles } from "twin.macro";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <div>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </div>
+);
 
-export default MyApp;
+export default App;
