1aa6124340d4977fe91c3415e774fee304350cac
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-11-16 21:55:43 +0200

    Storybook + twin.macro + emotions + nextjs

--- typescript/packages/lib-core/components/CardTailWindExample.tsx
+++ typescript/packages/lib-core/components/CardTailWindExample.tsx
@@ -4,12 +4,12 @@ export const CardTailWindExample = () => {
   return (
     <div tw="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
       <div tw="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto">
-        <div className="rounded-lg shadow-xs overflow-hidden">
-          <div className="p-4">
-            <div className="flex items-start">
-              <div className="flex-shrink-0">
+        <div tw="rounded-lg overflow-hidden">
+          <div tw="p-4">
+            <div tw="flex items-start">
+              <div tw="flex-shrink-0">
                 <svg
-                  className="h-6 w-6 text-green-400"
+                  tw="h-6 w-6 text-green-400"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -23,8 +23,8 @@ export const CardTailWindExample = () => {
                   />
                 </svg>
               </div>
-              <div className="ml-3 w-0 flex-1 pt-0.5">
-                <p className="text-sm font-medium text-gray-900">Successfully saved!</p>
+              <div tw="ml-3 w-0 flex-1 pt-0.5">
+                <p tw="text-sm font-medium text-gray-900">Successfully saved!</p>
                 <p className="mt-1 text-sm text-gray-500">Anyone with a link can now view this file.</p>
               </div>
               <div className="ml-4 flex-shrink-0 flex">
