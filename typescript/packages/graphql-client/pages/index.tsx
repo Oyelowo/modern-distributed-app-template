a8a292b0af7f5b2e33a365ddc5b2049afb757684
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2022-02-23 08:30:52 +0200

    Clean new graphql-client

---
+++ typescript/packages/graphql-client/pages/index.tsx
@@ -0,0 +1,10 @@
+export const ButtonBoop = () => {
+  return <button>Boop</button>;
+};
+
+export function getLowo() {
+  return "Oyelowo L'uomo Universale!";
+}
+
+// export {Button, TextField, CardTailWindExample} from "./components"
+export * from "../hooks/useInterval";
