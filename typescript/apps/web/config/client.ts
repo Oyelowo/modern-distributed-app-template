cd17d9e1cb4a6730e550045d1bf612bd963c5d1c
oyelowo <oyelowooyedayo@gmail.com>
2022-04-24 16:38:30 +0300

    Update client url

--- typescript/apps/web/config/client.ts
+++ typescript/apps/web/config/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient("http://localhost:8080/graphql", {
+export const client = new GraphQLClient("https://oyelowo.dev/graphql", {
   credentials: "include",
   headers: {},
 });
