b157b19936ab778a16ba153577113269f27bcb6c
oyelowo <oyelowooyedayo@gmail.com>
2022-06-26 09:20:51 +0300

    Some cleanups

--- typescript/apps/web/config/client.ts
+++ typescript/apps/web/config/client.ts
@@ -2,7 +2,8 @@ import { GraphQLClient } from "graphql-request";
 
 // Get this from infra kubernetes and also dynamically assign it depending on the environment
 // export const DOMAIN_BASE = "https://oyelowo.dev"
-export const DOMAIN_BASE = "http://api.localhost:8080";
+export const DOMAIN_BASE = "http://localhost:8080/api";
+// export const DOMAIN_BASE = "http://api.oyelowo.local:8080";
 
 export const client = new GraphQLClient(`${DOMAIN_BASE}/graphql`, {
   credentials: "include",
