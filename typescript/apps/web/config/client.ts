2150a90d48fee9ac5c4b36b1b062df3c11c9f585
oyelowo <oyelowooyedayo@gmail.com>
2022-06-26 18:04:09 +0300

    Import client host from infra module

--- typescript/apps/web/config/client.ts
+++ typescript/apps/web/config/client.ts
@@ -1,8 +1,9 @@
 import { GraphQLClient } from "graphql-request";
-
+import { hosts } from "../../../../kubernetes/resources/infrastructure/ingress/hosts"
 // Get this from infra kubernetes and also dynamically assign it depending on the environment
 // export const DOMAIN_BASE = "https://oyelowo.dev"
-export const DOMAIN_BASE = "http://localhost:8080/api";
+export const DOMAIN_BASE = `${hosts.local.api}/api`;
+// export const DOMAIN_BASE = "http://localhost:8080/api";
 // export const DOMAIN_BASE = "http://api.oyelowo.local:8080";
 
 export const client = new GraphQLClient(`${DOMAIN_BASE}/graphql`, {
