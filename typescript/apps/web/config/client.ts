d321d04a66bd1f0ae38bd80f4bba350601c05473
oyelowo <oyelowooyedayo@gmail.com>
2022-06-27 05:44:23 +0300

    Some cleanups

--- typescript/apps/web/config/client.ts
+++ typescript/apps/web/config/client.ts
@@ -1,8 +1,8 @@
 import { GraphQLClient } from "graphql-request";
-import { hosts } from "../../../../kubernetes/resources/infrastructure/ingress/hosts"
+// import { hosts } from "../../../../kubernetes/resources/infrastructure/ingress/hosts"
 
 // Get this from infra kubernetes and also dynamically assign it depending on the environment
-export const DOMAIN_BASE = hosts.local.apiUrl;
+export const DOMAIN_BASE = "http://localhost:8080/api";
 
 export const client = new GraphQLClient(`${DOMAIN_BASE}/graphql`, {
   credentials: "include",
