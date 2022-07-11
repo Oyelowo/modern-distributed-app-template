2a55cf3b100c470e938b1ab4082b4acabfa7ed89
oyelowo <oyelowooyedayo@gmail.com>
2022-07-11 20:26:24 +0300

    Eslint with mantine

--- typescript/packages/eslint-config-custom/index.js
+++ typescript/packages/eslint-config-custom/index.js
@@ -1,7 +1,26 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: [
+    "next",
+    "prettier",
+    "mantine",
+    // "plugin:@next/next/recommended",
+    "plugin:jest/recommended",
+    "plugin:storybook/recommended",
+    // "custom",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  plugins: ["testing-library", "jest"],
+  overrides: [
+    {
+      files: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
   },
 };
