f200879ea398c1ddc398b3185470a7c82e5dce40
oyelowo <oyelowooyedayo@gmail.com>
2022-07-12 11:42:19 +0300

    Fix tests

--- typescript/apps/web/utils/test-utils.tsx
+++ typescript/apps/web/utils/test-utils.tsx
@@ -1,15 +1,13 @@
-import React, { FC, ReactElement } from "react";
-import { render, RenderOptions } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { AppProps } from "next/app";
-import "../styles/globals.css";
-import { Provider } from "jotai";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import { useState } from "react";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { SSRProvider } from "react-aria";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
+import React, { FC, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '../styles/globals.css';
+import { Provider } from 'jotai';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
   // { Component, pageProps: { session, ...pageProps } }: AppProps
@@ -18,21 +16,17 @@ const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
     <Provider>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
-        <Hydrate>
-          <SSRProvider>{children}</SSRProvider>
-        </Hydrate>
+        <Hydrate>{children}</Hydrate>
       </QueryClientProvider>
     </Provider>
   );
 };
 
-const customRender = (
-  ui: ReactElement,
-  options?: Omit<RenderOptions, "wrapper">
-) => render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
 
-export * from "@testing-library/react";
-import "@testing-library/jest-dom";
+export * from '@testing-library/react';
+import '@testing-library/jest-dom';
 export { customRender as render };
 export { rest };
 export { setupServer };
