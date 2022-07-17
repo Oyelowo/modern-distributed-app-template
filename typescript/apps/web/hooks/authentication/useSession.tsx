ade3a483a85dbbc806ec3abcc732e36c646ce079
oyelowo <oyelowooyedayo@gmail.com>
2022-07-18 02:05:50 +0300

    Some more fixes

--- typescript/apps/web/hooks/authentication/useSession.tsx
+++ typescript/apps/web/hooks/authentication/useSession.tsx
@@ -1,5 +1,4 @@
 import { SessionQuery, useSessionQuery } from '@oyelowo/graphql-client';
-import { useRouter } from 'next/router';
 import { client } from '../../config/client';
 import { GraphqlErrorResponse, mapToServerData } from './helpers';
 import { UseSessionProps } from './useAuth';
@@ -10,15 +9,14 @@ export function useSession(props?: UseSessionProps) {
   // const forceUpdate = useForceUpdate()
   const { queryConfig = { staleTime: 0 } } = props ?? {};
 
-  const router = useRouter();
 
-  const { data, status, isLoading, isIdle, isFetching, error } = useSessionQuery<
+  const { data, status,  error } = useSessionQuery<
     SessionQuery,
     GraphqlErrorResponse
   >(client, undefined, {
     staleTime: 0,
     onSettled(data, error) {
-      if (queryConfig.onSettled) queryConfig.onSettled(data, error);
+      if (queryConfig?.onSettled) queryConfig?.onSettled(data, error);
       const hasError = !!error;
       // if (hasError) {
       //   router.push('/login/?error=SessionExpired');
