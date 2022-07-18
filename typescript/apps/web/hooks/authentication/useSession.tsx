7a532980ae9102b7858b3c0ea5faeb15b1afec5e
oyelowo <oyelowooyedayo@gmail.com>
2022-07-18 14:46:17 +0300

    Format code

--- typescript/apps/web/hooks/authentication/useSession.tsx
+++ typescript/apps/web/hooks/authentication/useSession.tsx
@@ -9,21 +9,21 @@ export function useSession(props?: UseSessionProps) {
   // const forceUpdate = useForceUpdate()
   const { queryConfig = { staleTime: 0 } } = props ?? {};
 
-
-  const { data, status,  error } = useSessionQuery<
-    SessionQuery,
-    GraphqlErrorResponse
-  >(client, undefined, {
-    staleTime: 0,
-    onSettled(data, error) {
-      if (queryConfig?.onSettled) queryConfig?.onSettled(data, error);
-      const hasError = !!error;
-      // if (hasError) {
-      //   router.push('/login/?error=SessionExpired');
-      // }
-      // forceUpdate();
-    },
-  });
+  const { data, status, error } = useSessionQuery<SessionQuery, GraphqlErrorResponse>(
+    client,
+    undefined,
+    {
+      staleTime: 0,
+      onSettled(data, error) {
+        if (queryConfig?.onSettled) queryConfig?.onSettled(data, error);
+        const hasError = !!error;
+        // if (hasError) {
+        //   router.push('/login/?error=SessionExpired');
+        // }
+        // forceUpdate();
+      },
+    }
+  );
 
   let mappedData = mapToServerData({
     status,
