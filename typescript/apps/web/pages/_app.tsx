77f0de656a469291a38cfa051b7dd4f077eb876a
oyelowo <oyelowooyedayo@gmail.com>
2022-07-18 15:53:22 +0300

    Cleanup apps component and some tods

--- typescript/apps/web/pages/_app.tsx
+++ typescript/apps/web/pages/_app.tsx
@@ -1,11 +1,10 @@
-import { Provider } from 'jotai';
+import { Provider, useAtom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { useEffect, useState } from 'react';
-import { useColorScheme } from '@mantine/hooks';
 import type { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
-
 // import { getCookie, setCookies } from 'cookies-next';
 import Head from 'next/head';
 import {
@@ -15,12 +14,17 @@ import {
   MantineThemeOverride,
 } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
 const overridenTheme: MantineThemeOverride = {
   primaryColor: 'cyan',
 };
 
+// If you dont want flickering of white before dark, u can use cookie for color
+// scheme. check mantine doc.
+// Server side rendering also solves this problem
+const colorAtom = atomWithStorage<'dark' | 'light'>('color-scheme', 'dark');
 function App({ Component, pageProps }: AppPropsWithLayout & { colorScheme: ColorScheme }) {
   /*
 Create a new QueryClient instance inside of your app, and on an instance ref (or in React state).
@@ -28,26 +32,16 @@ This ensures that data is not shared between different users and requests, while
 creating the QueryClient once per component lifecycle.
 */
   const [queryClient] = useState(() => new QueryClient());
+  const [colorScheme, setColorScheme] = useAtom(colorAtom);
+
   const getLayout = Component.getLayout ?? ((page) => page);
-  // const [theme, setTheme] = useAtom(pop);
-  // hook will return either 'dark' or 'light' on client
-  // and always 'light' during ssr as window.matchMedia is not available
 
-  // const { theme: colorScheme, toggleTheme: toggleColorScheme } = useThemeOfApp();
-  const preferredColorScheme = useColorScheme('dark');
-
-  // console.log('preferredColorScheme', preferredColorScheme);
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value ?? (colorScheme === 'dark' ? 'light' : 'dark'));
-  /*
-    Saving color scheme in cookie is the easiest way to prevent color scheme mismatch.
-    This strategy can be applied to any framework/library that has server side rendering support.
-    The following example shows how to store color scheme in cookie with Next.js:
-     */
-  // setCookies('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
 
   /*
+  TODO: Remove when fixed
+  Issue with react not matching what was rendered on the server in react 18
   https://stackoverflow.com/questions/71706064/react-18-hydration-failed-because-the-initial-ui-does-not-match-what-was-render
   */
   // START
@@ -63,17 +57,6 @@ creating the QueryClient once per component lifecycle.
   if (typeof window === 'undefined') {
     return <></>;
   }
-  // END
-
-  // return (
-  // const { Component, pageProps } = props;
-  // const [colorScheme, setColorScheme] = useState<ColorScheme>(pageProps.colorScheme);
-
-  // const toggleColorScheme = (value?: ColorScheme) => {
-  //   const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
-  //   setColorScheme(nextColorScheme);
-  //   // setCookies('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
-  // };
 
   return (
     <>
@@ -85,7 +68,7 @@ creating the QueryClient once per component lifecycle.
 
       <Provider>
         <QueryClientProvider client={queryClient}>
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+          <ReactQueryDevtools initialIsOpen={false} />
           <Hydrate state={pageProps?.dehydratedState}>
             <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
               <MantineProvider
@@ -118,61 +101,3 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default App;
-
-// App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-//   colorScheme: /*  getCookie('mantine-color-scheme', ctx) || */ 'dark',
-// });
-
-/*  TODO: PROTECTED ROUTES
-
-// pages/admin.jsx
-export default function AdminDashboard () {
-  const [session] = useSession()
-  // session is always non-null inside this page, all the way down the React tree.
-  return "Some super secret dashboard"
-}
-
-AdminDashboard.auth = true
-//pages/_app.jsx
-export default function App({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      {Component.auth
-        ? <Auth><Component {...pageProps} /></Auth>
-        : <Component {...pageProps} />
-      }
-    </SessionProvider>
-  )
-}
-
-function Auth({ children }) {
-  const [session, loading] = useSession()
-  const isUser = !!session?.user
-  React.useEffect(() => {
-    if (loading) return // Do nothing while loading
-    if (!isUser) signIn() // If not authenticated, force log in
-  }, [isUser, loading])
-
-  if (isUser) {
-    return children
-  }
-
-  // Session is being fetched, or no user.
-  // If no user, useEffect() will redirect.
-  return <div>Loading...</div>
-}
-It can be easily be extended/modified to support something like an options object. An example:
-
-// pages/admin.jsx
-AdminDashboard.auth = {
-  role: "admin",
-  loading: <AdminLoadingSkeleton/>,
-  unauthorized: "/login-with-different-user" // redirect to this url
-}
-Because of how _app is done, it won't unnecessarily contant the /api/auth/session endpoint for pages that do not require auth.
-
-TYPESCRIPT
-
-export default FooPage;
-
-*/
