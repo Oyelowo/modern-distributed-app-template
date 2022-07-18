6f0b2440136077d033411ae4275c8b74be8c56b8
oyelowo <oyelowooyedayo@gmail.com>
2022-07-18 14:12:30 +0300

    Fix storybook

---
+++ typescript/packages/ui/components/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export function Button(props: React.ReactComponentElement<"button">) {
+  return <button>Some Buttom</button>;
+}
--- typescript/packages/ui/stories/Button.stories.tsx
+++ typescript/packages/ui/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Button } from "../components";
+import { Button } from "../components/Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -12,26 +12,19 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>Do it</Button>
-);
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>Do it</Button>;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  className: ["btn-primary"],
-};
+Primary.args = {};
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  className: ["btn-secondary"],
   // isSmall: false,
 };
 
 export const Large = Template.bind({});
-Large.args = {
-  className: ["btn-primary"],
-};
+Large.args = {};
 
 export const Small = Template.bind({});
 Small.args = {};
