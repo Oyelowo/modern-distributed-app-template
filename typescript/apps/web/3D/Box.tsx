821a0d9e833dae135764d7cd87ccd214064e9d30
oyelowo <oyelowooyedayo@gmail.com>
2022-07-26 06:00:38 +0300

    Update react three component

--- typescript/apps/web/3D/Box.tsx
+++ typescript/apps/web/3D/Box.tsx
@@ -1,23 +1,27 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, ComponentPropsWithRef } from 'react';
 import { Canvas, useFrame, MeshProps } from '@react-three/fiber';
 
 function Box(props: MeshProps) {
   // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef<any>();
+  const ref = useRef<ComponentPropsWithRef<'mesh'>>();
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  useFrame((_state, _delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += 0.01;
+    }
+  });
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 3 : 2}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
+      onClick={(_event) => click(!clicked)}
+      onPointerOver={(_event) => hover(true)}
+      onPointerOut={(_event) => hover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
