617ee7389ff03b5bf039dcd540fe59c6a92acdd1
oyelowo <oyelowooyedayo@gmail.com>
2022-11-19 18:40:19 +0200

    Migrate ui lib

---
+++ typescript/lib-ui/index.tsx
@@ -0,0 +1,9 @@
+export const ButtonBoop = () => {
+  return <button>Boop</button>;
+};
+
+export function getLowo() {
+  return "Oyelowo L'uomo Universale!";
+}
+
+export * from "./hooks/useInterval";
