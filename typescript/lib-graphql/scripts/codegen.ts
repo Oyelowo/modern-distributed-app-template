14698740773f5ff6c12c75e87a4d10bb9a84d7c8
oyelowo <oyelowooyedayo@gmail.com>
2022-11-19 20:15:34 +0200

    Add an escape hatch for dealing with non deno-compatible gqp geneated files

--- typescript/lib-graphql/scripts/codegen.ts
+++ typescript/lib-graphql/scripts/codegen.ts
@@ -1,17 +1,23 @@
 import { CodegenConfig, generate } from "@graphql-codegen/cli";
 
-const config: CodegenConfig = {
+const GRAPHQL_GENEATED_CODE_PATH = "./generated/";
+
+export const config: CodegenConfig = {
   schema: "../../rust/graphql-surrealdb/generated/schema.graphql",
   documents: "./operations/*.graphql",
   watch: true,
   emitLegacyCommonJSImports: false,
   ignoreNoDocuments: true,
+  debug: true,
   generates: {
-    ["./src/"]: {
+    [GRAPHQL_GENEATED_CODE_PATH]: {
       plugins: [],
       preset: "client",
     },
   },
+  hooks: {
+    afterAllFileWrite: ["npx ts-node ./scripts/remove_unwanted_files.ts"],
+  },
 };
 
-generate(config, true).catch((e) => {});
+generate(config, true);
--- typescript/lib-graphql/src/index.ts
+++
@@ -1,4 +0,0 @@
-// deno-lint-ignore-file no-local
-
-export * from "./gql.js";
-export * from "./fragment-masking.js";
