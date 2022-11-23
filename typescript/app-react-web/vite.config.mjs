051bda041422b2135132d34b82d98429690fd7d5
oyelowo <oyelowooyedayo@gmail.com>
2022-11-23 19:29:49 +0200

    Update package json

--- typescript/app-react-web/vite.config.mjs
+++ typescript/app-react-web/vite.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "npm:vite";
-import react from "npm:@vitejs/plugin-react";
-import jotaiDebugLabel from "npm:jotai/babel/plugin-debug-label";
-import jotaiReactRefresh from "npm:jotai/babel/plugin-react-refresh";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
