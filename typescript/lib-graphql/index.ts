910250b4bc1b759db1309be93d20e70cdd225ab3
oyelowo <oyelowooyedayo@gmail.com>
2022-11-25 15:57:50 +0200

    Fix graphql query sync

--- typescript/lib-graphql/index.ts
+++ typescript/lib-graphql/index.ts
@@ -1,1 +1,17 @@
+import { ZeusScalars } from "./zeus/index.js";
 export * from "./zeus/index.js";
+import { string } from 'zod';
+
+const uuidSchema = string().uuid({ message: "Invalid uuid" });
+
+// TODO: Use proper encoding/decoding
+export const scalarsCustom = ZeusScalars({
+    UUID: {
+        encode: (e: unknown) => uuidSchema.parse(e),
+        decode: (e: unknown) => String(e),
+    },
+    DateTime: {
+        decode: (e: unknown) => new Date(e as string),
+        encode: (e: unknown) => (e as Date).toISOString(),
+    },
+});
--- typescript/lib-graphql/zeus/const.ts
+++ typescript/lib-graphql/zeus/const.ts
@@ -1,136 +1,130 @@
 /* eslint-disable */
 
-export const AllTypesProps: Record<string, any> = {
-	Address: {},
+export const AllTypesProps: Record<string,any> = {
+	Address:{
+
+	},
 	DateTime: `scalar.DateTime` as const,
-	Mutation: {
-		createUser: {
-			userInput: "UserInput",
+	Mutation:{
+		createUser:{
+			userInput:"UserInput"
 		},
-		signUp: {
-			user: "UserInput",
+		signUp:{
+			user:"UserInput"
 		},
-		signIn: {
-			signInCredentials: "SignInCredentials",
+		signIn:{
+			signInCredentials:"SignInCredentials"
 		},
-		createPost: {
-			post: "PostInput",
-		},
+		createPost:{
+			post:"PostInput"
+		}
 	},
 	OauthProvider: "enum" as const,
-	ObjectId: `scalar.ObjectId` as const,
-	PostInput: {},
-	Query: {
-		user: {
-			id: "ObjectId",
+	PostInput:{
+
+	},
+	Query:{
+		user:{
+			id:"UUID"
 		},
-		getUser: {
-			userBy: "UserBy",
+		getUser:{
+			userBy:"UserBy"
 		},
-		post: {
-			id: "ObjectId",
-		},
+		post:{
+			id:"UUID"
+		}
 	},
 	Role: "enum" as const,
-	SignInCredentials: {},
-	TokenType: "enum" as const,
-	UserBy: {
-		userId: "ObjectId",
-		address: "Address",
-	},
-	UserInput: {},
-};
+	SignInCredentials:{
 
-export const ReturnTypes: Record<string, any> = {
-	oneOf: {},
-	AccountOauth: {
-		id: "String",
-		displayName: "String",
-		email: "String",
-		emailVerified: "Boolean",
-		provider: "OauthProvider",
-		providerAccountId: "OauthProvider",
-		accessToken: "String",
-		refreshToken: "String",
-		expiresAt: "DateTime",
-		tokenType: "TokenType",
-		scopes: "String",
-		idToken: "String",
-		oauthToken: "String",
-		oauthTokenSecret: "String",
+	},
+	TokenType: "enum" as const,
+	UUID: `scalar.UUID` as const,
+	UserBy:{
+		userId:"UUID",
+		address:"Address"
+	},
+	UserInput:{
+
+	}
+}
+
+export const ReturnTypes: Record<string,any> = {
+	oneOf:{
+
+	},
+	AccountOauth:{
+		id:"String",
+		displayName:"String",
+		email:"String",
+		emailVerified:"Boolean",
+		provider:"OauthProvider",
+		providerAccountId:"OauthProvider",
+		accessToken:"String",
+		refreshToken:"String",
+		expiresAt:"DateTime",
+		tokenType:"TokenType",
+		scopes:"String",
+		idToken:"String",
+		oauthToken:"String",
+		oauthTokenSecret:"String"
 	},
 	DateTime: `scalar.DateTime` as const,
-	Mutation: {
-		createUser: "User",
-		signUp: "User",
-		signIn: "User",
-		signOut: "SignOutMessage",
-		createPost: "Post",
+	Mutation:{
+		createUser:"User",
+		signUp:"User",
+		signIn:"User",
+		signOut:"SignOutMessage",
+		createPost:"Post"
 	},
-	ObjectId: `scalar.ObjectId` as const,
-	Post: {
-		id: "ObjectId",
-		posterId: "ObjectId",
-		title: "String",
-		content: "String",
-		poster: "User",
+	Post:{
+		id:"UUID",
+		posterId:"UUID",
+		title:"String",
+		content:"String",
+		poster:"User"
 	},
-	SearchResult: {
-		"...on Photo": "Photo",
-		"...on Person": "Person",
+	Query:{
+		me:"User",
+		user:"User",
+		getUser:"User",
+		users:"User",
+		session:"Session",
+		post:"Post",
+		posts:"Post"
 	},
-	Person: {
-		name: "String",
-		age: "Int",
+	Session:{
+		userId:"UUID",
+		expiresAt:"DateTime"
 	},
-	Photo: {
-		height: "Int",
-		width: "Int",
+	SignOutMessage:{
+		message:"String",
+		userId:"UUID"
 	},
-	SearchQuery: {
-		firstSearchResult: "SearchResult",
+	Subscription:{
+		values:"Int"
 	},
-	Query: {
-		me: "User",
-		user: "User",
-		getUser: "User",
-		users: "User",
-		session: "Session",
-		post: "Post",
-		posts: "Post",
-		search: "SearchQuery",
-	},
-	Session: {
-		userId: "ObjectId",
-		expiresAt: "DateTime",
-	},
-	SignOutMessage: {
-		message: "String",
-		userId: "ObjectId",
-	},
-	Subscription: {
-		values: "Int",
-	},
-	User: {
-		id: "ObjectId",
-		createdAt: "DateTime",
-		username: "String",
-		firstName: "String",
-		lastName: "String",
-		city: "String",
-		email: "String",
-		emailVerified: "Boolean",
-		age: "Int",
-		socialMedia: "String",
-		roles: "Role",
-		accounts: "AccountOauth",
-		posts: "Post",
-		postCount: "Int",
-	},
-};
+	UUID: `scalar.UUID` as const,
+	User:{
+		id:"UUID",
+		createdAt:"DateTime",
+		username:"String",
+		firstName:"String",
+		lastName:"String",
+		city:"String",
+		email:"String",
+		emailVerified:"Boolean",
+		age:"Int",
+		socialMedia:"String",
+		roles:"Role",
+		accounts:"AccountOauth",
+		posts:"Post",
+		postCount:"Int"
+	}
+}
 
 export const Ops = {
-	mutation: "Mutation" as const,
+mutation: "Mutation" as const,
 	query: "Query" as const,
-	subscription: "Subscription" as const,
-};
+	subscription: "Subscription" as const
+}
