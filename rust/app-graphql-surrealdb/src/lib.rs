36c66a6b24059acdcd84bfc577315608ab5f672b
oyelowo <oyelowooyedayo@gmail.com>
2022-11-30 00:09:54 +0200

    Cargo format

--- rust/app-graphql-surrealdb/src/lib.rs
+++ rust/app-graphql-surrealdb/src/lib.rs
@@ -3,4 +3,3 @@
 pub mod app;
 pub mod handlers;
 pub mod utils;
- 
--- rust/lib-scripts/build.rs
+++ rust/lib-scripts/build.rs
@@ -1,13 +1,13 @@
 use std::path::Path;
 
-use app_graphql_surrealdb::{app::get_my_graphql_schema};
+use app_graphql_surrealdb::app::get_my_graphql_schema;
 use lib_common::utils;
 
 fn main() {
     // println!("cargo:rerun-if-changed=build.rs");
     let ts_path = "../../typescript/lib-graphql/generated/schema.graphql";
     println!("cargo:rerun-if-changed={ts_path}");
-    generate_schema( ts_path);
+    generate_schema(ts_path);
 }
 
 pub fn generate_schema(path: impl AsRef<Path>) {
