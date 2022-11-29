b9d98be80362cd28ddcbef7322fd553d6fef57e6
oyelowo <oyelowooyedayo@gmail.com>
2022-11-29 23:35:14 +0200

    More husky

--- rust/lib-scripts/build.rs
+++ rust/lib-scripts/build.rs
@@ -3,6 +3,5 @@ use app_graphql_surrealdb::utils::graphql::generate_schema;
 fn main() {
     // println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=../../typescript/lib-graphql/generated/schema.graphql");
-    let path = format!("../../typescript/lib-graphql/generated/schema.graphql");
-    generate_schema(path);
+    generate_schema("../../typescript/lib-graphql/generated/schema.graphql");
 }
