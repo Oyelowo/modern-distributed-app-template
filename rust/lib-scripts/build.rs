fb7f8bd3c4c4ce35ffeb3a40903d1e58886fad43
oyelowo <oyelowooyedayo@gmail.com>
2022-11-29 23:43:59 +0200

    Run if ts path changes

--- rust/lib-scripts/build.rs
+++ rust/lib-scripts/build.rs
@@ -2,6 +2,7 @@ use app_graphql_surrealdb::utils::graphql::generate_schema;
 
 fn main() {
     // println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=../../typescript/lib-graphql/generated/schema.graphql");
-    generate_schema("../../typescript/lib-graphql/generated/schema.graphql");
+    let ts_path = "../../typescript/lib-graphql/generated/schema.graphql";
+    println!("cargo:rerun-if-changed={ts_path}");
+    generate_schema(ts_path);
 }
