a7385b27168b6065e3fa9b044a9089b19a445dfb
oyelowo <oyelowooyedayo@gmail.com>
2022-12-08 14:25:18 +0200

    Update tests

--- typescript/app-web/tests/example.spec.ts
+++ typescript/app-web/tests/example.spec.ts
@@ -1,25 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("homepage has title and links to intro page", async ({ page }) => {
-	await page.goto("https://playwright.dev/");
-
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/Playwright/);
-
-	// create a locator
-	const getStarted = page.getByRole("link", { name: "Get started" });
-
-	// Expect an attribute "to be strictly equal" to the value.
-	await expect(getStarted).toHaveAttribute("href", "/docs/intro");
-
-	// Click the get started link.
-	await getStarted.click();
-
-	// Expects the URL to contain intro.
-	await expect(page).toHaveURL(/.*intro/);
-});
-
-test("vita", async ({ page }) => {
+test("Invoice page", async ({ page }) => {
 	await page.goto("http://localhost:5173/");
 
 	// Expect a title "to contain" a substring.
@@ -27,18 +8,16 @@ test("vita", async ({ page }) => {
 
 	// create a locator
 	const getStarted = page.getByRole("link", { name: "1 New Invoice" });
-	// const getStarted = page.getByRole("link", { name: "Get started" });
 
-	// // Expect an attribute "to be strictly equal" to the value.
+	// Expect an attribute "to be strictly equal" to the value.
 	await expect(getStarted).toHaveAttribute(
 		"href",
 		"/dashboard/invoices$invoiceId",
 	);
 
-	// // Click the get started link.
+	// Click the get started link.
 	await getStarted.click();
 
-	// // Expects the URL to contain intro.
-	// await expect(page).toHaveURL(/.*/);
+	// Expects the URL to contain intro.
 	await expect(page).toHaveURL(/.*invoiceId/);
 });
