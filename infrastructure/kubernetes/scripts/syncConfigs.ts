7acee9c5686124ceee19795de7de3791ddb2202d
oyelowo <oyelowooyedayo@gmail.com>
2022-12-14 00:32:03 +0200

    Cleanup sync condif

--- infrastructure/kubernetes/scripts/syncConfigs.ts
+++ infrastructure/kubernetes/scripts/syncConfigs.ts
@@ -1,11 +1,9 @@
 import { PlainSecretsManager } from "./utils/plainSecretsManager.js";
 import { syncCrdsCode } from "./utils/syncCrdsCode.js";
 import { syncEtcHostsWithCustomHosts } from "./utils/syncEtcHostsWithCustomHosts.js";
-import { syncHelmChartTypesDeclarations } from "./utils/syncHelmChartTypesDeclarations.js";
 
 async function main() {
 	syncEtcHostsWithCustomHosts();
-	syncHelmChartTypesDeclarations();
 
 	PlainSecretsManager.syncAll();
 	syncCrdsCode();
