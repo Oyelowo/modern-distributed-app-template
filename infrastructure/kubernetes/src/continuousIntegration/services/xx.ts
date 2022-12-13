33cb258364c66194b51ee10703cdb99cd1f0cfd5
oyelowo <oyelowooyedayo@gmail.com>
2022-12-14 01:05:36 +0200

    Add basic example

--- infrastructure/kubernetes/src/continuousIntegration/services/xx.ts
+++ infrastructure/kubernetes/src/continuousIntegration/services/xx.ts
@@ -5,9 +5,14 @@ import {
 	Workflow,
 	WorkflowTemplate,
 } from "../../../generatedCode/crds/argoproj/v1alpha1/index.js";
+import { HttpWorkflowsArgoprojIoWorkflowsJson } from "../../../generatedCode/missingCrdSchemas/argo-workflows.js";
 // import { WorkflowTemplate } from "../../../generatedCode/crds/argoproj/v1alpha1/workflowTemplate.js";
 
-new WorkflowTemplate("ere", { apiVersion: "argoproj.io/v1alpha1", spec: {} });
+new WorkflowTemplate("ere", {
+	apiVersion: "argoproj.io/v1alpha1",
+	kind: "WorkflowTemplate",
+	spec: {},
+} satisfies HttpWorkflowsArgoprojIoWorkflowsJson);
 new Sensor("", {
 	apiVersion: "argoproj.io/v1alpha1",
 	kind: "Sensor",
